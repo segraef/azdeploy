ee2d483480c31c7026979093221a3c830722cec6
Sebastian Gräf <segraef@microsoft.com>
2020-03-23 09:12:27 +0100

    indent

--- .github/actions/azdeploy/entry.ps1
+++ .github/actions/azdeploy/entry.ps1
@@ -18,30 +18,31 @@ if (-not $resourceGroupCommand -or ($resourceGroupCommand -like "create")) {
   if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
     if ($resourceGroupLocation ) {
       New-AzResourceGroup -Name $resourceGroupName -Location "$resourceGroupLocation"
-      if (-not $parametersFile) {
-        Write-Output "Parameters file parametersFile does not exists." 
-      }
-      else {
-        if ($templateFile) {
-          $DeploymentInputs = @{
-            ResourceGroupName     = "$resourceGroupName"
-            TemplateFile          = "$workspace/$templateFile"
-            TemplateParameterFile = "$workspace/$parametersFile"
-            Mode                  = "Incremental"
-            Verbose               = $true
-            ErrorAction           = "Stop"
-          }
-        
-          New-AzResourceGroupDeployment @DeploymentInputs
-        }
-        else {
-          Write-Output "Template file templateFile does not exists." 
-        }
-      }
     }
     else {
       Write-Output "resourceGroupLocation is not set."
       exit
+    }
+  } else {
+    if (-not $parametersFile) {
+      Write-Output "Parameters file parametersFile does not exists." 
+    }
+    else {
+      if ($templateFile) {
+        $DeploymentInputs = @{
+          ResourceGroupName     = "$resourceGroupName"
+          TemplateFile          = "$workspace/$templateFile"
+          TemplateParameterFile = "$workspace/$parametersFile"
+          Mode                  = "Incremental"
+          Verbose               = $true
+          ErrorAction           = "Stop"
+        }
+      
+        New-AzResourceGroupDeployment @DeploymentInputs
+      }
+      else {
+        Write-Output "Template file templateFile does not exists." 
+      }
     }
   }
 }
