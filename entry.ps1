99197c1c8112f5241c84c2d7386d76ee341fcb39
Sebastian <sebastian@graef.io>
2020-03-29 13:41:24 +0200

    added Uri support (#3)

--- entry.ps1
+++ entry.ps1
@@ -3,45 +3,91 @@ Param(
   [string]$resourceGroupLocation,
   [string]$resourceGroupCommand,
   [string]$templateFile,
-  [string]$parametersFile
+  [string]$parametersFile,
+  [string]$templateUri,
+  [string]$parametersUri
 )
 
 $context = Get-AzContext
 if (!$context) {
-  Write-Output "No Azure context found! Please make sure azlogin has run before."
+  Write-Output "##########`nNo Azure context found! Please make sure azlogin has run before.`n##########"
   exit
 } 
 
 if (-not $resourceGroupName) {
-  Write-Output "resourceGroupName is not set."
+  Write-Output "##########`nresourceGroupName is not set.`n##########"
   exit
 }
-
-if ($resourceGroupCommand -and ($resourceGroupCommand -like "create")) {
-  Write-Output "Executing commands to Create/Update resource group."
-  if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
-    if ($resourceGroupLocation) {
-      New-AzResourceGroup -Name $resourceGroupName -Location "$resourceGroupLocation"
+Try {
+  if ($resourceGroupCommand -and ($resourceGroupCommand -like "create")) {
+    Write-Output "##########`nExecuting commands to create/update resource group $resourceGroupName ...`n"
+    if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
+      if ($resourceGroupLocation) {
+        New-AzResourceGroup -Name $resourceGroupName -Location "$resourceGroupLocation" -Force
+      }
     }
-  } elseif ($templateFile -and $parametersFile) {
-    $DeploymentInputs = @{
-      Name                  = "$($env:GITHUB_WORKFLOW)-$($env:GITHUB_ACTOR)-$(Get-Date -Format yyyyMMddHHMMss)"
-      ResourceGroupName     = "$resourceGroupName"
-      TemplateFile          = "$templateFile"
-      TemplateParameterFile = "$parametersFile"
-      Mode                  = "Incremental"
-      Verbose               = $true
-      ErrorAction           = "Stop"
-    }
+    if ($templateFile -and $parametersFile) {
+      Write-Output "##########`nDeploying with templateFile and parametersFile in $resourceGroupName ...`n##########"
+      Write-Output "templateFile: $templateFile"
+      Write-Output "parametersFile: $parametersFile"
+      $DeploymentInputs = @{
+        Name                  = "$($env:GITHUB_WORKFLOW)-$($env:GITHUB_ACTOR)-$(Get-Date -Format yyyyMMddHHMMss)"
+        ResourceGroupName     = "$resourceGroupName"
+        TemplateFile          = "$templateFile"
+        TemplateParameterFile = "$parametersFile"
+        Mode                  = "Incremental"
+        Verbose               = $true
+        ErrorAction           = "Stop"
+      }
     
-    New-AzResourceGroupDeployment @DeploymentInputs
+      New-AzResourceGroupDeployment @DeploymentInputs
+    }
+    elseif ($templateUri -and $parametersUri) {
+      Write-Output "##########`nDeploying with templateUri and parametersUri in $resourceGroupName ...`n##########"
+      Write-Output "templateUri: $templateUri"
+      Write-Output "parametersUri: $parametersUri"
+      $DeploymentInputs = @{
+        Name                 = "$($env:GITHUB_WORKFLOW)-$($env:GITHUB_ACTOR)-$(Get-Date -Format yyyyMMddHHMMss)"
+        ResourceGroupName    = "$resourceGroupName"
+        TemplateUri          = "$templateUri"
+        TemplateParameterUri = "$parametersUri"
+        Mode                 = "Incremental"
+        Verbose              = $true
+        ErrorAction          = "Stop"
+      }
+    
+      New-AzResourceGroupDeployment @DeploymentInputs
+    }
+    elseif ($templateUri -and $parametersFile) {
+      Write-Output "##########`nDeploying with templateUri and parametersFile in $resourceGroupName ...`n##########"
+      Write-Output "templateUri: $templateUri"
+      Write-Output "parametersFile: $parametersFile"
+      $DeploymentInputs = @{
+        Name                  = "$($env:GITHUB_WORKFLOW)-$($env:GITHUB_ACTOR)-$(Get-Date -Format yyyyMMddHHMMss)"
+        ResourceGroupName     = "$resourceGroupName"
+        TemplateUri           = "$templateUri"
+        TemplateParameterFile = "$parametersFile"
+        Mode                  = "Incremental"
+        Verbose               = $true
+        ErrorAction           = "Stop"
+      }
+    
+      New-AzResourceGroupDeployment @DeploymentInputs
+    }
+    else {
+      Write-Output "##########`nTemplate or parameters file does not exist. ...`n##########"
+    }
+  }
+  elseif ($resourceGroupCommand -like "delete") {
+    Write-Output "resourceGroupCommand is set to 'delete'. Removing $resourceGroupName now. "
+    Remove-AzResourceGroup -Name $resourceGroupName -Force
   }
   else {
-    Write-Output "Template or parameters file does not exist." 
+    Write-Output "##########`nSomething went wrong ...`n##########"
   }
-} elseif ($resourceGroupCommand -like "delete") {
-  Write-Output "resourceGroupCommand is set to 'delete'. Removing $resourceGroupName now. "
-  Remove-AzResourceGroup -Name $resourceGroupName -Force
-} else {
-  Write-Output "Something went wrong."
+}
+Catch {
+  $_.Exception.Message
+  $_.Exception.ItemName
+  Throw
 }
